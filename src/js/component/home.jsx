b6d08baa59e8d95d210797bd5f18247fad46eb72
Jessan23 <jessicaanairayagrez@gmail.com>
2024-10-28 04:06:29 +0000

    inicie con la clase grabada

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -5,6 +5,24 @@ import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+
+setInterval(()=>{
+	const palabra=["Hola", "Mundo", "Programacion", "Javascript"]
+	const IndiceAleatorio = Math.floor(Math.random() *palabra.length)
+	console.log(palabra[IndiceAleatorio]);
+	
+},1000) 
+
+
+let sec = 0
+setInterval(()=>{
+	const one= Math.floor((sec/1)%10)
+	console.log(one)
+	sec+= 1
+}, 1000)
+
+
+
 	return (
 		<div className="text-center">
 			<h1 className="text-center mt-5">Hello Rigo!</h1>
