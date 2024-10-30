d49af8da0fb25f724f1821aad89a7ff22fd2ec07
Jessan23 <jessicaanairayagrez@gmail.com>
2024-10-30 03:16:57 +0000

    me falta arreglar el icono

--- src/js/component/card.jsx
+++ src/js/component/card.jsx
@@ -1,24 +1,22 @@
 import React from "react";
-function Card(props){
-    return(
-    <div className="card" style={{ 
-        backgroundColor: 'black', 
-        color: 'white', 
-        height: '100px', 
-        width: '100px', 
-        display: 'flex', 
-        alignItems: 'center', 
-        justifyContent: 'center',
-        margin: '0'
-    }}>
-        <div className="card-body" style={{ textAlign: 'center' }}>
-        {props.isClock && <i className="fa-solid fa-clock" style={{ fontSize: '2rem', marginBottom: '0.5rem' }}></i>}
 
-            <h5 className="card-title" style={{ fontSize: '2rem', margin: 0 }}>{props.numero}</h5>
+const Card = (props) => {
+    return (
+        <div className="card" style={{ width: "19rem", backgroundColor: 'black', color: 'white', margin: '0' }}>
+            <div className="card-body" style={{ textAlign: 'center' }}>
+                {props.isClock && (
+                    <i 
+                        className="fa-solid fa-clock-rotate-lef"
+                        style={{ fontSize: '19rem' }} 
+                         
+                    ></i>
+                )}
+                <h5 className="card-title" style={{ fontSize: '2rem', margin: 0 }}>
+                    {props.numero}
+                </h5>
+            </div>
         </div>
-    </div>
-    )
+    );
 }
-    
 
-export default Card
+export default Card;
--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,54 +1,54 @@
-import React, { useState, useEffect } from "react";
-import Card from "./card"
+import React from "react";
+import Card from "./card.jsx";
 
+let seconds = 0;
+let interval;
 
-
-const Home = () => {
-
-	const [sec, setSec] = useState(0);
-	const [digits, setDigits] = useState([]);
-
-	useEffect(() => {
-
-
-		const interval = setInterval(() => {
-			setSec(prevSec => prevSec + 1);
-		}, 1000);
-		return () => clearInterval(interval);
-	}, []);
-
-	useEffect(() => {
-		const newDigits = [
-			Math.floor((sec / 100000) % 10),
-			Math.floor((sec / 10000) % 10),
-			Math.floor((sec / 1000) % 10),
-			Math.floor((sec / 100) % 10),
-
-
-			Math.floor((sec / 10) % 10),
-			Math.floor(sec % 10),
-		];
-
-		setDigits(newDigits)
-	}, [sec]);
-
-return(
-
-
-<div className="container-fluid mt-9">
-			<div className="row justify-content-center">
-				{digits.map((digit, index) => (
-					<div key={index} className="col-2" style={{ padding: '0' }}>
-						<Card numero={digit} 
-						isClock={index === 0}
-						/>
-					</div>
-				))}
-			</div>
-		</div>
-	);
+const updateDigits = (newSeconds) => {
+  return [
+    Math.floor((newSeconds / 100000) % 10),
+    Math.floor((newSeconds / 10000) % 10),
+    Math.floor((newSeconds / 1000) % 10),
+    Math.floor((newSeconds / 100) % 10),
+    Math.floor((newSeconds / 10) % 10),
+    Math.floor(newSeconds % 10)
+  ];
 };
 
 
+const startTimer = (setDigits) => {
+  interval = setInterval(() => {
+    seconds += 1;
+    const newDigits = updateDigits(seconds);
 
-export default Home
+    console.log(newDigits);
+    setDigits(newDigits);
+  }, 1000);
+};
+
+const Home = () => {
+  const [digits, setDigits] = React.useState(Array(7).fill(0));
+
+
+  React.useEffect(() => {
+    startTimer(setDigits);
+    return () => clearInterval(interval);
+  }, []);
+
+  return (
+    <div className="container-fluid mt-4">
+            <div className="row justify-content-center">
+                {digits.map((digit, index) => (
+                    <div key={index} className="col-2" style={{ padding: "0" }}>
+                        <Card isClock={index === "isClock"} numero={index ==="isClock"  ? '' : digit} />
+                    
+
+
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Home;
